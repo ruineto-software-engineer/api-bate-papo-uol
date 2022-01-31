a88637547670b3392fe3c43a43cb1ea3277a830c
Rui Neto <ruineto11@gmail.com>
2022-01-31 15:19:35 -0300

    Add the data sanitization, route DELETE /messages/ID_OF_MESSAGE and PUT /messages/ID_OF_MESSAGE

--- back-bate-papo-uol/app.js
+++ back-bate-papo-uol/app.js
@@ -1,11 +1,15 @@
 import express, { json } from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectID, ObjectId } from "mongodb";
 import cors from 'cors';
 import dayjs from "dayjs";
 import joi from 'joi';
+import { stripHtml } from "string-strip-html";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+
+/* MongoDB - Data Base Connection */
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
@@ -32,14 +36,15 @@ app.post('/participants', async (req, res) => {
   }
 
   try {
-/*     await db.collection('users').deleteMany({});
-    await db.collection('messages').deleteMany({}); */
-
     const loggedUsers = await db.collection('users').find({}).toArray();
     const loggedUsersCollection = loggedUsers.map(user => user.name);
-    const result = loggedUsersCollection.find(loggedUser => loggedUser === user.name);
-    if(result === undefined){
-      await db.collection('users').insertOne(user);
+    const loggedUserSearch = loggedUsersCollection.find(loggedUser => loggedUser === user.name);
+    if(loggedUserSearch === undefined){
+      const userSanitazed = { 
+        name: stripHtml(user.name).result.trim(),
+        lastStatus: user.lastStatus
+      };
+      await db.collection('users').insertOne(userSanitazed);
 
       let status = {
         from: user.name,
@@ -94,7 +99,14 @@ app.post('/messages', async (req, res) => {
     }
 
     message.time = dayjs(Date.now()).format('HH:mm:ss');
-    await db.collection('messages').insertOne(message);
+    const messageSanitazed = {
+      from: stripHtml(message.from).result.trim(),
+      to: stripHtml(message.to).result.trim(),
+      text: stripHtml(message.text).result.trim(),
+      type: stripHtml(message.type).result.trim(),
+      time: message.time
+    };
+    await db.collection('messages').insertOne(messageSanitazed);
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
@@ -135,6 +147,81 @@ app.get('/messages', async (req, res) => {
   }
 });
 
+app.delete('/messages/:idMessage', async (req, res) => {
+  const user = req.header('User');
+  const idMessage = req.params.idMessage;
+
+  try {
+    const searchedMessage = await db.collection('messages').findOne({ _id: new ObjectId(idMessage) });
+    if(!searchedMessage){
+      res.sendStatus(404);
+      return;
+    }
+
+    if(searchedMessage.from !== user){
+      res.sendStatus(401);
+      return;
+    }
+
+    await db.collection('messages').deleteOne({ _id: searchedMessage._id });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/messages/:idMessage', async (req, res) => {
+  const idMessage = req.params.idMessage;
+  const from = req.header('User');
+  const message = { from, ...req.body };
+  
+  try {
+    const loggedUsers = await db.collection('users').find({}).toArray();
+    const loggedUsersCollection = loggedUsers.map(user => user.name);
+  
+    const messageSchema = joi.object({
+      from: joi.string().valid( ...loggedUsersCollection ).required(),
+      to: joi.string().required(),
+      text: joi.string().required(),
+      type: joi.string().valid('message', 'private_message').required()
+    });
+    const validation = messageSchema.validate(message, { abortEarly: false });
+    if (validation.error) {
+      res.status(422).send(validation.error.details.map(error => error.message));
+      return;
+    }
+
+    message.time = dayjs(Date.now()).format('HH:mm:ss');
+    const updatedMessageSanitazed = {
+      from: stripHtml(message.from).result.trim(),
+      to: stripHtml(message.to).result.trim(),
+      text: stripHtml(message.text).result.trim(),
+      type: stripHtml(message.type).result.trim(),
+      time: message.time
+    };
+
+    const searchedMessage = await db.collection('messages').findOne({ _id: new ObjectId(idMessage) });
+    if(!searchedMessage){
+      res.sendStatus(404);
+      return;
+    }
+
+    if(searchedMessage.from !== from){
+      res.sendStatus(401);
+      return;
+    }
+
+    await db.collection('messages').updateOne(
+      { _id: searchedMessage._id },
+      { $set: updatedMessageSanitazed }
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 
 /* Status Route */
